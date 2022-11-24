32e957a0575183aac659c6cce2c85dcfc9b81a98
alan <kno12350@gmail.com>
2022-11-24 21:42:07 +0900

    기본문법 다 구현해서 올림 부트스트랩까지

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,44 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <nav class="navbar navbar-expand-lg bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">WOLF</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        
+      </ul>
+      <form class="d-flex" role="search">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
+<div class="container mt-4">
+  <h5>React 개발자의 블로그 입니다.</h5>
+  <p> - Vue로 만들었음 - </p>
+</div>
+
+<List :블로그글="블로그글"/>
+
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import List from './components/list.vue';
+import data from './assets/blog.js';
 
 export default {
   name: 'App',
+  data() {
+    return {
+      블로그글 : data,
+    }
+  },
   components: {
-    HelloWorld
+    List : List,
   }
 }
 </script>
@@ -21,6 +50,5 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/list.vue
@@ -0,0 +1,25 @@
+<template>
+  <div v-for="(a,i) in 블로그글" :key="i">
+    <h4>{{블로그글[i].title}}</h4>
+    <p>{{블로그글[i].content}}</p>
+    <p>{{블로그글[i].date}}</p>
+  </div>
+  
+</template>
+
+<script>
+export default {
+    data()  {
+        return {
+
+        }
+    },
+    props : {
+        블로그글 : Array,
+    }
+}
+</script>
+
+<style>
+
+</style>
